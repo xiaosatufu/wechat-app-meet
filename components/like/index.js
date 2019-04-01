f3720f96332c4ba3eed530204e5052f2ac5253ed
xiaosatufu@163.com <xiaosatufu@163.com>
2019-04-01 20:43:05 +0800

    commit stash

--- components/like/index.js
+++ components/like/index.js
@@ -4,16 +4,18 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like:{
+      type:Boolean
+    },
+    count:{
+      type:Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    like:false,
-    count1:11,
-    count2:22,
     yesSrc:'images/like.png',
     noSrc:'images/like@dis.png'
   },
@@ -24,6 +26,15 @@ Component({
   methods: {
     onLike(e){
       console.log(e)
+      let like = this.properties.like
+      let count = this.properties.count
+
+      count = like?count-1:count+1
+      this.setData({
+        like:!like,
+        count:count
+      })
+
     }
   }
 })
