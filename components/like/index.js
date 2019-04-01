49851f8ec406b8be939b8c2a99074504088e00f2
yongshuang <yongshuang@gdgyun.com>
2019-04-01 17:37:41 +0800

    commit movie music storage

--- components/like/index.js
+++ components/like/index.js
@@ -4,16 +4,22 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like:{
+      type:Boolean
+    },
+    count:{
+      type:Number,
+      value:0
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    like:false,
-    count1:11,
-    count2:22,
+    // like:false,
+    // count1:11,
+    // count2:22,
     yesSrc:'images/like.png',
     noSrc:'images/like@dis.png'
   },
@@ -23,7 +29,19 @@ Component({
    */
   methods: {
     onLike(e){
-      console.log(e)
+      let like = this.properties.like
+      let count = this.properties.count
+
+      this.setData({
+        count:like?count-1:count+1,
+        like:!like
+      })
+
+      let behavior = this.properties.like?'like':'cancel'
+      this.triggerEvent('like',{
+        behavior: behavior
+      },{})
+
     }
   }
 })
--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,66 +1,122 @@
 // pages/classic/classic.js
+// import {HTTP} from '../../util/http.js'
+// let http = new HTTP()
+import {
+  ClassicModel
+} from '../../models/classic.js'
+import {
+  LikeModel
+} from '../../models/like.js'
+let classicModel = new ClassicModel()
+let likeModel = new LikeModel()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    classic: null,
+    latest: true,
+    first: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    classicModel.getLatest((res) => {
+      // console.log(res)
+      this.setData({
+        classic: res
+      })
+    })
+    // http.request({
+    //   url:'classic/latest',
+    //   success:(res)=>{
+    //     console.log(res)
+    //   }
+    // })
+  },
+
+  onLike(e) {
+    console.log(e)
+    let behavior = e.detail.behavior
+    likeModel.like(behavior, this.data.classic.id, this.data.classic.type)
 
   },
+  onNext(e) {},
+  onPrev(e) {
+    let index = this.data.classic.index
+    classicModel.getPrevious(index, (res) => {
+      // console.log(res)
+      this.setData({
+        classic:res,
+        latest:classicModel.isLatest(res.index),
+        first:classicModel.isFirst(res.index)
+      })
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    // let promise = new Promise((resolve, reject) => {
+    //   wx.request({
+    //     url: 'http://bl.7yue.pro/v1/classic/latest',
+    //     header: {
+    //       appkey: 'KOLDaSADSDLWWbF'
+    //     },
+    //     success: (res) => {
+    //       resolve(res)
+    //     }
+    //   })
+    // })
+    // promise.then((res)=>{
+    //   console.log(res)
+    // })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
